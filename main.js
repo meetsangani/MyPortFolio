0466a3c6270ba457bb1cbdc44ba042f03fc23142
Meet Sangani <109665965+meetsangani@users.noreply.github.com>
2024-08-02 20:00:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed(".text", {
+        strings: ["Web Developer", "Android Developer", "Software Engineer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 2000,
+        loop: true
+    });
+});
